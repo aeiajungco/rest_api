b0790171fc4619b91db407a9d65b3940849b6bcf
Angelika Jungco <angelika.jungco@usjr.edu.ph>
2022-07-08 22:21:37 +0800

    created HTTP Methods and Status Codes

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,24 +1,34 @@
+import 'dart:convert';
+import 'package:rest_api/models/api_response.dart';
 import 'package:rest_api/models/notes_for_listing.dart';
+import 'package:http/http.dart' as http;
 
 class NotesService {
-  List<NoteForListing> getNotesList() {
-    return [
-    new NoteForListing(
-        noteID: "1",
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-        noteTitle: "Note 1"),
-    new NoteForListing(
-        noteID: "2",
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-        noteTitle: "Note 2"),
-    new NoteForListing(
-        noteID: "2",
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-        noteTitle: "Note 2"),
-  ];
+  static const API = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
+  static const headers = {'apiKey': 'a954f9fb-fedb-4bab-b2ac-131e7eb8b0ee'};
 
+  Future<APIResponse<List<NoteForListing>>> getNotesList() {
+    return http.get(Uri.parse(API + '/notes'), headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final notes = <NoteForListing>[];
+        for (var item in jsonData) {
+          final note = NoteForListing(
+              noteID: item['noteID'],
+              noteTitle: item['noteTitle'],
+              createDateTime: DateTime.parse(item['createDateTime']),
+              latestEditDateTime: item['latestEditDateTime'] != null
+                  ? DateTime.parse(item['latestEditDateTime'])
+                  : null);
+          notes.add(note);
+        }
+        return APIResponse<List<NoteForListing>>(
+          data: notes,
+        );
+      }
+      return APIResponse<List<NoteForListing>>(
+          error: true, errorMessage: 'An error occured');
+    }).catchError((_) => APIResponse<List<NoteForListing>>(
+        error: true, errorMessage: 'An error occured'));
   }
 }
